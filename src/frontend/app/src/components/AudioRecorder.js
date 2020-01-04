97b65e3ebc0507d96a51c9cb67a899099cda6179
gab <gabriel.mougard@gmail.com>
2020-01-04 16:43:15 +0100

    frontend : we have the recording interface

---
+++ src/frontend/app/src/components/AudioRecorder.js
@@ -0,0 +1,134 @@
+import React, { Component } from 'react'
+import { ReactMic } from 'react-mic'
+
+require('./styles.scss')
+
+class AudioRecorder extends Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      downloadLinkURL: null,
+      isRecording: false,
+      recordingStarted: false,
+      recordingStopped: false
+    }
+  }
+
+  stopRecording= () => {
+    this.setState({ isRecording: false })
+  }
+
+  onSave=(blobObject) => {
+    this.setState({
+      downloadLinkURL: blobObject.blobURL
+    })
+  }
+
+  onStart=() => {
+    console.log('You can tap into the onStart callback')
+  }
+
+  onStop= (blobObject) => {
+    this.setState({ blobURL: blobObject.blobURL })
+  }
+
+  onData(recordedBlob){
+    // console.log('ONDATA CALL IS BEING CALLED! ', recordedBlob);
+  }
+
+  onBlock() {
+    alert('ya blocked me!')
+  }
+
+  startRecording= () => {
+    this.setState({
+      isRecording: true,
+      recordingInSession: true,
+      recordingStarted: true,
+      recordingStopped: false,
+      isPaused: false
+    })
+  }
+
+  stopRecording=() => {
+    this.setState({
+      isRecording: false,
+      recordingInSession: false,
+      recordingStarted: false,
+      recordingStopped: true
+    })
+  }
+
+  render() {
+    const {
+      blobURL,
+      downloadLinkURL,
+      isRecording,
+      recordingInSession,
+      recordingStarted,
+      recordingStopped
+    } = this.state
+
+    const recordBtn = recordingInSession ? "fa disabled fa-record-vinyl fa-fw" : "fa fa-record-vinyl fa-fw"
+    const stopBtn = !recordingStarted ? "fa disabled fa-stop-circle" : "fa fa-stop-circle"
+    const downloadLink = recordingStopped ? "fa fa-download" : "fa disabled fa-download"
+
+    return (
+      <div>
+        <div id="project-wrapper">
+          <div id="project-container">
+            <div id="overlay" />
+            <div id="content">
+              <h2>RapGO.io - generate a rap with your voice !</h2>
+              <ReactMic
+                className="oscilloscope"
+                record={isRecording}
+                backgroundColor="#333333"
+                visualSetting="sinewave"
+                audioBitsPerSecond={128000}
+                onStop={this.onStop}
+                onStart={this.onStart}
+                onSave={this.onSave}
+                onData={this.onData}
+                onBlock={this.onBlock}
+                onPause={this.onPause}
+                strokeColor="#0096ef"
+              />
+              <div id="oscilloscope-scrim">
+                {!recordingInSession && <div id="scrim" />}
+              </div>
+              <div id="controls">
+                <div className="column active">
+                  <i
+                    onClick={this.startRecording}
+                    className={recordBtn}
+                    aria-hidden="true"
+                  />
+                </div>
+                <div className="column">
+                  <i
+                    onClick={this.stopRecording}
+                    className={stopBtn}
+                    aria-hidden="true"
+                  />
+                </div>
+                <div className="column download">
+                  <a
+                    className={downloadLink}
+                    href={downloadLinkURL}
+                    download={`recording.webm`}
+                  />
+                </div>
+              </div>
+            </div>
+            <div id="audio-playback-controls">
+              <audio ref="audioSource" controls="controls" src={blobURL} controlsList="nodownload"/>
+            </div>
+          </div>
+        </div>
+      </div>
+    )
+  }
+}
+
+export default AudioRecorder
