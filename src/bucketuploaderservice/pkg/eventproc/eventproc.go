2b94d46e8829c2e8738ba9b63242976fb400a01c
gab <gabriel.mougard@gmail.com>
2020-01-08 11:58:58 +0100

    continuing eventproc.go

--- src/bucketuploaderservice/pkg/eventproc/eventproc.go
+++ src/bucketuploaderservice/pkg/eventproc/eventproc.go
@@ -3,6 +3,7 @@ package eventproc
 import (
 	"log"
 	"os"
+	"strings"
 
 	"rapGO.io/src/bucketuploaderservice/pkg/bucket"
 	"rapGO.io/src/bucketuploaderservice/pkg/kafka"
@@ -36,17 +37,29 @@ func (ep *EventProcessor) ProcessEvents() {
 		}
 	}
 }
-
+// So far handleEvent only receive `toBucket` events
+//
 func (ep *EventProcessor) handleEvent(event kafka.Event) {
 	//Unmarshal content of event
 	var eventUUID string
 	var filePrefix string
 	var heartbeatDesc string
 	var filenameToBucket string
-	switch e := event.(type) {
-	case *eventmodel.ToBucketEvent:
-		log.Printf("event %s created: %s", e.ID, e)
 
+	switch e := event.(type) {
+	case *events.ToBucketEvent:
+		log.Printf("event %s created: %s", e.EventUUID, e)
+		eventUUID = e.EventUUID
+		filenameToBucket = e.EventFilename
+		filePrefix = strings.Split(e.EventFilename,"_")[0]
+		switch filePrefix {
+		case "input":
+			heartbeatDesc = "Saving raw data to cloud..."
+		case "output":
+			heartbeatDesc = "Saving generated data to cloud..."
+		default:
+			heartbeatDesc = "Internal error. File prefix not recognized."
+		}
 	default:
 		log.Printf("unknown event type: %T", e)
 	}
@@ -69,7 +82,5 @@ func (ep *EventProcessor) handleEvent(event kafka.Event) {
 			log.Printf("The heartbeat for UUID: %s couldn't be sent", eventUUID)
 		}
 	}(ep.EventEmitter, filenameToBucket, eventUUID, heartbeatDesc)
-
-
 
 }
--- src/bucketuploaderservice/pkg/kafka/events/heartbeat.go
+++ src/bucketuploaderservice/pkg/kafka/events/heartbeat.go
@@ -1,12 +1,20 @@
 package events
 
+import (
+	"os"
+)
+
 // describe the heartbeat event to send to the `heartbeat_<UUID>` Kafka topic
 type HeartbeatEvent struct {
 	EventUUID     string `json:"eventUUID"`
-	HeartbeatDesc string `josn:"heartbeatDesc"`
+	HeartbeatDesc string `json:"heartbeatDesc"`
 }
 
 //EventName returns the event's name
 func (he *HeartbeatEvent) EventName() string {
-	return "heartbeat_"+he.EventUUID
+	if prefix, ok := os.LookupEnv("KAFKA_TOPICPREFIX_HEARTBEAT"); ok {
+		return prefix+he.EventUUID
+	} else {
+		return ""
+	}
 }
